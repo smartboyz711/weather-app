848cba0da8bad6b860c2e751164fe711067d9222
smartboyz711 <30695719+smartboyz711@users.noreply.github.com>
2017-08-09 16:14:39 +0700

    boby

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const request = require('request')
+const request = require('request');
 
 request({
     url : 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
     json : true
 },(error, response, body) => {
-    console.log(JSON.stringify(response, undefined, 2))
+    console.log('address : '+body.results[0].formatted_address);
+    console.log('lat : '+body.results[0].geometry.location.lat);
+    console.log('lng : '+body.results[0].geometry.location.lng);
 })
-
