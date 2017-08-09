dcee69dbc0bf066be91d9b983fd3bc624bc78252
smartboyz711 <30695719+smartboyz711@users.noreply.github.com>
2017-08-09 17:08:40 +0700

    yargs

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a:{
+            demand : true,
+            alias : 'address',
+            describe : 'Address to fetch weather for',
+            string : true
+        }
+    })
+    .help()
+    .alias('help','h')
+    .argv;
+
+console.log(argv);
 
 request({
     url : 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
