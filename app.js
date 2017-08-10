eeb3b647a9a27ec8cd89128a7d29e713d67cd81d
smartboyz711 <30695719+smartboyz711@users.noreply.github.com>
2017-08-10 09:17:21 +0700

    handler error

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const argv = yargs
     .alias('help','h')
     .argv;
 
-console.log('command : '+argv);
+console.log('command : '+argv.a);
 var AddressEncode = encodeURIComponent(argv.a);
 console.log('AddressEncode : '+AddressEncode);
 
@@ -23,11 +23,15 @@ request({
     url : 'https://maps.googleapis.com/maps/api/geocode/json?address='+AddressEncode,
     json : true
 },(error, response, body) => {
-    if(body){
+    if(error){
+        console.log('Unable to connnect to Google Servers.');
+    }else if(body.status == 'ZERO_RESULTS'){
+        console.log('Unable to find your addresss');
+    }else if(body.status == 'OK'){
         console.log('address : '+body.results[0].formatted_address);
         console.log('lat : '+body.results[0].geometry.location.lat);
         console.log('lng : '+body.results[0].geometry.location.lng);
     }else{
-        console.log(error);
+        console.log('UnKnown Status from Google Api');
     }
 });
