b23734d5cfab8d08ecbd47e4928c6291ecaf46e4
smartboyz711 <30695719+smartboyz711@users.noreply.github.com>
2017-08-10 22:58:20 +0700

    label

--- playground/promise2.js
+++ playground/promise2.js
@@ -29,7 +29,7 @@ var geocodeAddress = (address) => {
     });
 };
 
-geocodeAddress('19146').then((location) => {
+geocodeAddress('ekkamai 30').then((location) => {
     console.log(JSON.stringify(location,undefined,2));
 },(errorMessage) => {
     console.log(errorMessage);
